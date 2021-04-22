1d4c68e7f99ecd50982de31190d8ad68a250f460
Alex Melnikov <alexmlnkv@gmail.com>
2021-04-22 10:52:38 +0300

    fix(script.js) исправить баг

--- lesson18_hard/script.js
+++ lesson18_hard/script.js
@@ -8,17 +8,19 @@ function setText() {
   paragraph.textContent = input.value;
 }
 
-function debounce(f, t) {
-  return args => {
-    const previousCall = this.lastCall;
-    this.lastCall = Date.now();
-    if (previousCall && ((this.lastCall - previousCall) <= t)) {
-      clearTimeout(this.lastCallTimer);
-    }
-    this.lastCallTimer = setTimeout(() => f(args), t);
-  };
+class Debounce {
+  constructor(fn, time) {
+    return args => {
+      const previousCall = this.lastCall;
+      this.lastCall = Date.now();
+      if (previousCall && ((this.lastCall - previousCall) <= time)) {
+        clearTimeout(this.lastCallTimer);
+      }
+      this.lastCallTimer = setTimeout(() => fn(args), time);
+    };
+  }
 }
 
-const debouncedLogger = debounce(setText, 300);
+const debouncedLogger = new Debounce(setText, 300);
 
 input.addEventListener('input', debouncedLogger);
